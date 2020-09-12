877e6b3156e85d006229a2aebe1c5ce220b18c70
shakti-aryan <beingshakti@gmail.com>
2020-09-12 13:48:51 +0530

    Deployment config to standard environment

--- sg-bookmark/src/main/java/com/socgen/bookmark/sgbookmark/SgBookmarkApplication.java
+++ sg-bookmark/src/main/java/com/socgen/bookmark/sgbookmark/SgBookmarkApplication.java
@@ -2,10 +2,17 @@ package com.socgen.bookmark.sgbookmark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SgBookmarkApplication {
+public class SgBookmarkApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SgBookmarkApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SgBookmarkApplication.class, args);
 	}
