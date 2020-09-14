bc47885734b361a85ded6996b4ea806baed8ed17
shakti-aryan <beingshakti@gmail.com>
2020-09-14 21:06:14 +0530

    swagger config

--- src/main/java/com/socgen/bookmark/sgbookmark/conf/WebSecurityConfiguration.java
+++ src/main/java/com/socgen/bookmark/sgbookmark/conf/WebSecurityConfiguration.java
@@ -2,22 +2,27 @@ package com.socgen.bookmark.sgbookmark.conf;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableOAuth2Sso
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-   @Override
-   protected void configure(HttpSecurity http) throws Exception {
-      http
-         .csrf()
-         .disable()
-         .antMatcher("/**")
-         .authorizeRequests()
-         .antMatchers("/", "/index.html")
-         .permitAll()
-         .anyRequest()
-         .authenticated();
-   }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().antMatcher("/**").authorizeRequests().antMatchers("/", "/index.html").permitAll()
+				.anyRequest().authenticated();
+		http.exceptionHandling()
+				.defaultAuthenticationEntryPointFor(getRestAuthenticationEntryPoint(),
+						new AntPathRequestMatcher("/api/v1/**"));
+	}
+
+	private AuthenticationEntryPoint getRestAuthenticationEntryPoint() {
+		return new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED);
+	}
+
 }
---
+++ src/main/java/com/socgen/bookmark/sgbookmark/controller/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.socgen.bookmark.sgbookmark.controller;
+
+import java.security.Principal;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+	
+	
+	@PostMapping(value = "/user")
+	public Principal user(Principal principal) {
+      return principal;
+	}
+	
+	
+	@GetMapping(value = "/getCurrentContextUser")
+	public String userInfo() {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		return auth.getName();
+	}
+	
+}
